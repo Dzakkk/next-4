7801c0999e6b59a2501f8b03fc2e35ab28674a28
Dzakkk <fpdzak@gmail.com>
2025-01-21 15:53:18 +0700

    ini lahh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
 'use client'
-import { Link } from '@chakra-ui/next-js'
-import SidebarWithHeader from './components/sidebar'
-import { Button, Card, Stack, Text } from '@chakra-ui/react'
-import { FcLock } from 'react-icons/fc'
+
 import Home from './(pages)/home/page'
 
 export default function Page() {
