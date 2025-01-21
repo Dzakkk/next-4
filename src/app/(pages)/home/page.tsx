697094e5a3d9d571aff157ca5b89c71635b63d7b
Dzakkk <fpdzak@gmail.com>
2025-01-21 10:03:05 +0700

    add chakra ui

---
+++ src/app/(pages)/home/page.tsx
@@ -0,0 +1,14 @@
+import { Button, HStack, VStack } from "@chakra-ui/react"
+
+const HomePage = () => {
+    return (
+        <div className="">
+            <VStack>
+                <Button>Click me</Button>
+                <Button>Click me</Button>
+            </VStack>
+        </div>
+    )
+}
+
+export default HomePage
---
+++ src/app/(pages)/setting/page.tsx
@@ -0,0 +1,7 @@
+const Setting = () => {
+    return (
+        <div className="">ini setting</div>
+    )
+}
+
+export default Setting
