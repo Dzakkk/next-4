6454318960de1e9bccb8ff516d3d54418b0853d7
Dzakkk <fpdzak@gmail.com>
2025-01-23 08:10:56 +0700

    local crud

--- src/app/(pages)/user/page.tsx
+++ src/app/(pages)/user/page.tsx
@@ -5,11 +5,11 @@ import SmallWithSocial from "@/app/components/footer";
 import Header from "@/app/components/heading";
 import SidebarWithHeader from "@/app/components/sidebar";
 import { TableLayout, TableControl } from "@/app/components/table";
-import { fakeUsers, UserInterface } from "@/types/User";
-import { Box } from "@chakra-ui/react";
+import { initValueUser, UserInterface } from "@/types/User";
+import { Box, Button, FormControl, FormLabel, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Radio, RadioGroup, useDisclosure } from "@chakra-ui/react";
 import { ColumnDef, getCoreRowModel, getPaginationRowModel, PaginationState, useReactTable } from "@tanstack/react-table";
-import { info } from "console";
-import { access } from "fs";
+import * as Yup from "yup";
+import { useFormik } from "formik";
 import { useEffect, useMemo, useState } from "react";
 
 interface LinkItemProps {
@@ -27,15 +27,17 @@ const User = () => {
     const [dataUsers, setDataUsers] = useState<UserInterface[]>([])
     const [{ pageIndex, pageSize }, setPagination] = useState<PaginationState>({ pageIndex: 0, pageSize: 10 })
     const [totalData, setTotalData] = useState<number>(0)
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const [isEdit, setIsEdit] = useState<boolean>(false);
 
     useEffect(() => {
-        setDataUsers(fakeUsers)
-        setTotalData(fakeUsers.length)
+        setDataUsers(dataUsers)
+        setTotalData(dataUsers.length)
     }, [totalData])
 
     useEffect(() => {
-        setDataUsers(fakeUsers)
-    }, [dataUsers])
+        setDataUsers(dataUsers)
+    }, [])
 
     const fetchDataOption = {
         pageIndex,
@@ -67,13 +69,29 @@ const User = () => {
                 footer: (props) => props.column.id
             },
             {
-                accessorFn: (row) => row.role,
-                id: "role",
+                accessorFn: (row) => row.status,
+                id: "status",
                 header: "Status",
                 cell: (info) => info.getValue(),
                 footer: (props) => props.column.id
             },
-
+            {
+                id: "actions",
+                header: "Actions",
+                cell: (info) => {
+                    const row = info.row.original;
+                    return (
+                        <Box display="flex" gap="2">
+                            <Button size="sm" colorScheme="yellow" onClick={() => handleEdit(row)}>
+                                Edit
+                            </Button>
+                            <Button size="sm" colorScheme="red" onClick={() => handleDelete(row.id)}>
+                                Delete
+                            </Button>
+                        </Box>
+                    );
+                },
+            },
         ],
         []
     );
@@ -81,7 +99,7 @@ const User = () => {
 
 
     const table = useReactTable({
-        data: fakeUsers,
+        data: dataUsers,
         columns: columns,
         // pageCount: totalData,
         state: {
@@ -96,17 +114,138 @@ const User = () => {
         manualPagination: false
     })
 
+    const handleEdit = (user: UserInterface) => {
+        formik.setValues(user);
+        setIsEdit(true);
+        onOpen();
+    };
+
+    const handleDelete = (id: number) => {
+        const filteredUsers = dataUsers.filter((user) => user.id !== id);
+        setDataUsers(filteredUsers);
+    };
+
+    const handleSubmit = (values: UserInterface) => {
+        if (isEdit) {
+            setDataUsers((prevUsers) =>
+                prevUsers.map((user) => (user.id === values.id ? { ...values } : user))
+            );
+        } else {
+            setDataUsers((prevUsers) => [
+                ...prevUsers,
+                { ...values, id: prevUsers.length + 1 },
+            ]);
+        }
+        formik.resetForm();
+        setIsEdit(false);
+        onClose();
+    };
+
+    const userSchema = Yup.object().shape({
+        name: Yup.string().required("Name is required"),
+        email: Yup.string().email("Invalid email").required("Email is required"),
+        status: Yup.string().required("Status is required"),
+    });
+
+    const formik = useFormik({
+        initialValues: initValueUser,
+        validationSchema: userSchema,
+        onSubmit: handleSubmit,
+    });
+
     return (
         <SidebarWithHeader>
             <Header>
                 <BreadCrumbs items={LinkItems} />
             </Header>
-            <Box mb={"20"}>
+            <Button onClick={onOpen} mt={"4"} colorScheme={"blue"} shadow={"lg"}>
+                Add Data
+            </Button>
+            <Box mb={20}>
                 <TableLayout table={table} />
-                <TableControl table={table}/>
+                <TableControl table={table} />
             </Box>
-
             <SmallWithSocial />
+            {/* Modal */}
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>{isEdit ? "Edit User" : "Add User"}</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody pb={6}>
+                        <form onSubmit={formik.handleSubmit}>
+                            <FormControl
+                                isInvalid={formik.touched.name && Boolean(formik.errors.name)}
+                                mb={4}
+                            >
+                                <FormLabel>Name</FormLabel>
+                                <Input
+                                    id="name"
+                                    name="name"
+                                    type="text"
+                                    placeholder="Enter name"
+                                    value={formik.values.name}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                />
+                                {formik.touched.name && formik.errors.name && (
+                                    <Box color="red.500">{formik.errors.name}</Box>
+                                )}
+                            </FormControl>
+                            <FormControl
+                                isInvalid={formik.touched.email && Boolean(formik.errors.email)}
+                                mb={4}
+                            >
+                                <FormLabel>Email</FormLabel>
+                                <Input
+                                    id="email"
+                                    name="email"
+                                    type="email"
+                                    placeholder="Enter email"
+                                    value={formik.values.email}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                />
+                                {formik.touched.email && formik.errors.email && (
+                                    <Box color="red.500">{formik.errors.email}</Box>
+                                )}
+                            </FormControl>
+                            <FormControl
+                                isInvalid={formik.touched.status && Boolean(formik.errors.status)}
+                                mb={4}
+                            >
+                                <FormLabel>Status</FormLabel>
+                                <RadioGroup
+                                    id="status"
+                                    name="status"
+                                    value={formik.values.status}
+                                    onChange={(value) => formik.setFieldValue("status", value)}
+                                >
+                                    <Box display="flex" flexDirection="column" gap={2}>
+                                        <Radio   value="user">User</Radio>
+                                        <Radio value="killer">Killer</Radio>
+                                    </Box>
+                                </RadioGroup>
+                                {formik.touched.status && formik.errors.status && (
+                                    <Box color="red.500">{formik.errors.status}</Box>
+                                )}
+                            </FormControl>
+
+                            <ModalFooter>
+                                <Button
+                                    colorScheme="blue"
+                                    type="submit"
+                                    mr={3}
+                                    isDisabled={!formik.dirty || !formik.isValid}
+                                >
+                                    Save
+                                </Button>
+                                <Button onClick={onClose}>Cancel</Button>
+                            </ModalFooter>
+                        </form>
+                    </ModalBody>
+                </ModalContent>
+            </Modal>
         </SidebarWithHeader>
     );
 };
--- src/types/User.ts
+++ src/types/User.ts
@@ -1,129 +1,136 @@
 export interface UserInterface {
-    id: number;
-    name: string;
-    email: string;
-    role: string;
+  id: number;
+  name: string;
+  email: string;
+  status: string;
 }
 
-export const fakeUsers: UserInterface[] = [
-    {
-        id: 1,
-        name: "John Doe",
-        email: "johndoe@example.com",
-        role: 'user',
-    },
-    {
-        id: 2,
-        name: "Jane Smith",
-        email: "janesmith@example.com",
-        role: 'killer',
-    },
-    {
-        id: 3,
-        name: "Alice Johnson",
-        email: "alicejohnson@example.com",
-        role: 'user',
-    },
-    {
-        id: 4,
-        name: "Bob Brown",
-        email: "bobbrown@example.com",
-        role: 'killer',
-    },
-    {
-        id: 5,
-        name: "Charlie Wilson",
-        email: "charliewilson@example.com",
-        role: 'user',
-    },
-    {
-        id: 6,
-        name: "Emily Davis",
-        email: "emilydavis@example.com",
-        role: 'user',
-    },
-    {
-        id: 7,
-        name: "Frank Harris",
-        email: "frankharris@example.com",
-        role: 'killer',
-    },
-    {
-        id: 8,
-        name: "Grace Miller",
-        email: "gracemiller@example.com",
-        role: 'user',
-    },
-    {
-        id: 9,
-        name: "Henry Moore",
-        email: "henrymoore@example.com",
-        role: 'killer',
-    },
-    {
-        id: 10,
-        name: "Ivy Taylor",
-        email: "ivytaylor@example.com",
-        role: 'user',
-    },
-    {
-        id: 11,
-        name: "king John Doe",
-        email: "johndoe@example.com",
-        role: 'user',
-    },
-    {
-        id: 12,
-        name: "king ane Smith",
-        email: "janesmith@example.com",
-        role: 'killer',
-    },
-    {
-        id: 13,
-        name: "king Alice Johnson",
-        email: "alicejohnson@example.com",
-        role: 'user',
-    },
-    {
-        id: 14,
-        name: "king Bob Brown",
-        email: "bobbrown@example.com",
-        role: 'killer',
-    },
-    {
-        id: 15,
-        name: "king Charlie Wilson",
-        email: "charliewilson@example.com",
-        role: 'user',
-    },
-    {
-        id: 16,
-        name: "king Emily Davis",
-        email: "emilydavis@example.com",
-        role: 'user',
-    },
-    {
-        id: 17,
-        name: "king Frank Harris",
-        email: "frankharris@example.com",
-        role: 'killer',
-    },
-    {
-        id: 18,
-        name: "king Grace Miller",
-        email: "gracemiller@example.com",
-        role: 'user',
-    },
-    {
-        id: 19,
-        name: "king Henry Moore",
-        email: "henrymoore@example.com",
-        role: 'killer',
-    },
-    {
-        id: 20,
-        name: "king Taylor",
-        email: "ivytaylor@example.com",
-        role: 'user',
-    },
-];
+export const initValueUser: UserInterface = {
+  id: 0,
+  name: "",
+  email: "",
+  status: "",
+};
+
+// export const fakeUsers: UserInterface[] = [
+//   {
+//     id: 1,
+//     name: "John Doe",
+//     email: "johndoe@example.com",
+//     status: "user",
+//   },
+//   {
+//     id: 2,
+//     name: "Jane Smith",
+//     email: "janesmith@example.com",
+//     status: "killer",
+//   },
+//   {
+//     id: 3,
+//     name: "Alice Johnson",
+//     email: "alicejohnson@example.com",
+//     status: "user",
+//   },
+//   {
+//     id: 4,
+//     name: "Bob Brown",
+//     email: "bobbrown@example.com",
+//     status: "killer",
+//   },
+//   {
+//     id: 5,
+//     name: "Charlie Wilson",
+//     email: "charliewilson@example.com",
+//     status: "user",
+//   },
+//   {
+//     id: 6,
+//     name: "Emily Davis",
+//     email: "emilydavis@example.com",
+//     status: "user",
+//   },
+//   {
+//     id: 7,
+//     name: "Frank Harris",
+//     email: "frankharris@example.com",
+//     status: "killer",
+//   },
+//   {
+//     id: 8,
+//     name: "Grace Miller",
+//     email: "gracemiller@example.com",
+//     status: "user",
+//   },
+//   {
+//     id: 9,
+//     name: "Henry Moore",
+//     email: "henrymoore@example.com",
+//     status: "killer",
+//   },
+//   {
+//     id: 10,
+//     name: "Ivy Taylor",
+//     email: "ivytaylor@example.com",
+//     status: "user",
+//   },
+//   {
+//     id: 11,
+//     name: "king John Doe",
+//     email: "johndoe@example.com",
+//     status: "user",
+//   },
+//   {
+//     id: 12,
+//     name: "king ane Smith",
+//     email: "janesmith@example.com",
+//     status: "killer",
+//   },
+//   {
+//     id: 13,
+//     name: "king Alice Johnson",
+//     email: "alicejohnson@example.com",
+//     status: "user",
+//   },
+//   {
+//     id: 14,
+//     name: "king Bob Brown",
+//     email: "bobbrown@example.com",
+//     status: "killer",
+//   },
+//   {
+//     id: 15,
+//     name: "king Charlie Wilson",
+//     email: "charliewilson@example.com",
+//     status: "user",
+//   },
+//   {
+//     id: 16,
+//     name: "king Emily Davis",
+//     email: "emilydavis@example.com",
+//     status: "user",
+//   },
+//   {
+//     id: 17,
+//     name: "king Frank Harris",
+//     email: "frankharris@example.com",
+//     status: "killer",
+//   },
+//   {
+//     id: 18,
+//     name: "king Grace Miller",
+//     email: "gracemiller@example.com",
+//     status: "user",
+//   },
+//   {
+//     id: 19,
+//     name: "king Henry Moore",
+//     email: "henrymoore@example.com",
+//     status: "killer",
+//   },
+//   {
+//     id: 20,
+//     name: "king Taylor",
+//     email: "ivytaylor@example.com",
+//     status: "user",
+//   },
+// ];
